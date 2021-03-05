3aa8683f8c06f86134d0ca8e08c44d11e6e23f6b
Triet Nguyen <nguytrie@flip1.engr.oregonstate.edu>
2021-03-04 21:16:02 -0800

    Updated MATLAB

--- EdgeDetection/Edge_Detection.m
+++ EdgeDetection/Edge_Detection.m
@@ -1,15 +1,38 @@
 type sobel
+type gCodeReader
 
-im = imread('path.jpg');
+filename = input('enter file name:', 's');
+
+im = imread(filename);
 
 image(im);
 
 gray = (0.2989 * double(im(:,:,1)) + 0.5870 * double(im(:,:,2)) + 0.1140 * double(im(:,:,3)))/255;
-
+% gray = (0.2989 * double(im(:,:)) + 0.5870 * double(im(:,:)) + 0.1140 * double(im(:,:)))/255;
 edgeIm = sobel(gray, 0.7);
 
-im3 = repmat(edgeIm, [1 1 3]);
+im3 = repmat(edgeIm, [1 1]);
 
 image(im3);
 
-fprint();
+writematrix(im3, 'im3.xlsx');
+
+[row,col] = size(im3);
+
+fileID = fopen('gcode.txt','w');
+fprintf(fileID, '%s\n','G90'); 
+% fprintf(fileID, '%s\n','G0 X0 Y0');
+rapide = 'G0';
+
+% G-Code Generator
+for i = 1:col
+    for j = 1:row
+        if (im3(i,j) == 0)|(j == 1)
+            fprintf(fileID, 'T1\nM6\n');
+            fprintf(fileID, '%s X%d Y%d\n','G0', i, j);
+        elseif im3(i,j) == 255;
+            fprintf(fileID, 'T0\nM6\n');
+            fprintf(fileID, '%s X%d Y%d\n','G0', i, j);
+        end
+    end
+end
