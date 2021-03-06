174eec51a1d6dfe6669205a72b24d064e5a758ea
ninjaco1 <nguyean6@users.noreply.github.com>
2021-03-05 23:46:51 -0800

    update edge detection python

--- EdgeDetection/edgeDetection.py
+++ EdgeDetection/edgeDetection.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('images.png',0)
+img = cv2.imread('test.jpg',0)
 edges = cv2.Canny(img,100,200)
 
 plt.subplot(121),plt.imshow(img,cmap = 'gray')
@@ -10,9 +10,37 @@ plt.title('Original Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(edges,cmap = 'gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
 
-# print(edges)
+# print(edges[0])
 # for arr in edges:
 #     print(arr)
+
+
+edges = list(edges)
+# clear top
+for _ in range(len(edges)):
+    if np.all((edges[0] == 0)) == True:
+        # print(np.all((edges[0] == 0)))
+        edges.pop(0)
+    else:
+        break
+
+# clear buttom 
+for _ in range(len(edges)):
+    if np.all((edges[len(edges)-1] == 0)) == True:
+        edges.pop(len(edges)-1)
+    else:
+        break
+
+# clear left
+# for i in range(len(edges)):
+#     total = 0
+#     for j in range(len(edges)):
+#         total += edges[j][0]
+#     if total == 0:
+#         for j in range(len(edges)):
+#             edges[j].pop(0)
+#     else:
+#         break
 
 f = open("gcode.txt","w")
 
